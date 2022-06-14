9045aa426a73b8953220cdc42cc3b7fb8710e5ad
Diego Hartwig <hartwig.diego@gmail.com>
2022-06-14 20:53:31 -0300

    Criando estrutura inicial do template

---
+++ TemplateCleanArchMvc.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TemplateCleanArchMvc.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
