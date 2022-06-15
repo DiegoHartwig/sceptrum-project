76565ab961aa19db454b9e146e7da33f365c6260
Diego Hartwig <hartwig.diego@gmail.com>
2022-06-14 21:17:34 -0300

    Criando as primeiras entidades

---
+++ TemplateCleanArchMvc.Domain/Entities/Categoria.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace TemplateCleanArchMvc.Domain.Entities
+{
+    public sealed class Categoria
+    {
+        public int Id { get; private set; }
+        public string Descricao { get; private set; }
+
+        public Categoria(string descricao)
+        {
+            Descricao = descricao;
+        }
+
+        public Categoria(int id, string descricao)
+        {
+            Id = id;
+            Descricao = descricao;
+        }
+
+        public ICollection<Produto> Produtos { get; set; }
+    }
+}
---
+++ TemplateCleanArchMvc.Domain/Entities/Produto.cs
@@ -0,0 +1,15 @@
+﻿namespace TemplateCleanArchMvc.Domain.Entities
+{
+    public sealed class Produto
+    {
+        public int Id { get; private set; }
+        public string Descricao { get; private set; }
+        public string Nome { get; private set; }
+        public string Preco { get; private set; }
+        public int Estoque { get; private set; }
+        public string Imagem { get; private set; }
+
+        public int CategoriaId { get; private set; }
+        public Categoria Categoria { get; private set; }
+    }
+}
