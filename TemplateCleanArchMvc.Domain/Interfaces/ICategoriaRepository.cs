e27b21a5747edfaa0d1288fece79c51b51b98722
Diego Hartwig <hartwig.diego@gmail.com>
2022-06-15 20:28:51 -0300

    Criando as interfaces

---
+++ TemplateCleanArchMvc.Domain/Interfaces/ICategoriaRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TemplateCleanArchMvc.Domain.Entities;
+
+namespace TemplateCleanArchMvc.Domain.Interfaces
+{
+    public interface ICategoriaRepository
+    {
+        Task<IEnumerable<Categoria>> BuscarCategoriasAsync();
+        Task<Categoria> BuscarPeloIdAsync(int? id);
+        Task<Categoria> CriarAsync(Categoria categoria);
+        Task<Categoria> AtualizarAsync(Categoria categoria);
+        Task<Categoria> RemoverAsync(Categoria categoria);
+    }
+}
---
+++ TemplateCleanArchMvc.Domain/Interfaces/IProdutoRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TemplateCleanArchMvc.Domain.Entities;
+
+namespace TemplateCleanArchMvc.Domain.Interfaces
+{
+    public interface IProdutoRepository
+    {
+        Task<IEnumerable<Produto>> BuscarProdutosAsync();
+        Task<Produto> BuscarPeloIdAsync(int? id);
+        Task<Produto> BuscarPelaCategoriaAsync(int? id);
+        Task<Produto> CriarAsync(Produto produto);
+        Task<Produto> AtualizarAsync(Produto produto);
+        Task<Produto> RemoverAsync(Produto produto);
+    }
+}
