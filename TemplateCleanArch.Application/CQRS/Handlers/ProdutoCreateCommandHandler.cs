7de1a32665ae30552b007776fbc52065a4b4766f
Diego Hartwig <hartwig.diego@gmail.com>
2022-06-17 17:36:37 -0300

    CQRS, implementando os handlers para os commands e queries

---
+++ TemplateCleanArch.Application/CQRS/Handlers/ProdutoCreateCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using TemplateCleanArch.Application.CQRS.Commands;
+using TemplateCleanArch.Domain.Entities;
+using TemplateCleanArch.Domain.Interfaces;
+
+namespace TemplateCleanArch.Application.CQRS.Handlers
+{
+    public class ProdutoCreateCommandHandler : IRequestHandler<ProdutoCreateCommand, Produto>
+    {
+        private readonly IProdutoRepository _produtoRepository;
+
+        public ProdutoCreateCommandHandler(IProdutoRepository produtoRepository)
+        {
+            _produtoRepository = produtoRepository;
+        }
+
+        public async Task<Produto> Handle(ProdutoCreateCommand request, CancellationToken cancellationToken)
+        {
+            var produto = new Produto(
+                request.Descricao,
+                request.Nome,
+                request.Preco,
+                request.Estoque,
+                request.Imagem);
+
+            if(produto == null)
+			{
+                throw new ApplicationException($"Ocorreu um erro.");
+			}
+            else
+			{
+                produto.CategoriaId = request.CategoriaId;
+                return await _produtoRepository.CriarAsync(produto);
+			}
+        }
+    }
+}
---
+++ TemplateCleanArch.Application/CQRS/Handlers/ProdutoRemoveCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using TemplateCleanArch.Application.CQRS.Commands;
+using TemplateCleanArch.Domain.Entities;
+using TemplateCleanArch.Domain.Interfaces;
+
+namespace TemplateCleanArch.Application.CQRS.Handlers
+{
+    public class ProdutoRemoveCommandHandler : IRequestHandler<ProdutoRemoveCommand, Produto>
+    {
+        private readonly IProdutoRepository _produtoRepository;
+
+        public ProdutoRemoveCommandHandler(IProdutoRepository produtoRepository)
+        {
+            _produtoRepository = produtoRepository ??
+                throw new ArgumentNullException(nameof(produtoRepository));
+        }
+
+        public async Task<Produto> Handle(ProdutoRemoveCommand request, CancellationToken cancellationToken)
+        {
+            var produto = await _produtoRepository.BuscarPeloIdAsync(request.Id);
+
+            if (produto == null)
+            {
+                throw new ApplicationException($"Ocorreu um erro.");
+            }
+            else
+            {
+                var resultado = await _produtoRepository.RemoverAsync(produto);
+                return resultado;
+            }
+        }
+    }
+}
