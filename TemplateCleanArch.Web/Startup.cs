af5eef1ce77d76a29610a9996ebf38c74e5da3a6
Diego Hartwig <hartwig.diego@gmail.com>
2022-06-19 19:09:13 -0300

    Implementando metodos para inserir, editar, deletar e apresentar detalhes de uma categoria

--- TemplateCleanArch.Web/Startup.cs
+++ TemplateCleanArch.Web/Startup.cs
@@ -49,7 +49,7 @@ namespace TemplateCleanArch.Web
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Produtos}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
