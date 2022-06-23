d4836360841fcd935f7ad1949f442a296f901e33
Diego Hartwig <hartwig.diego@gmail.com>
2022-06-22 21:28:44 -0300

    Finalizando a implementação da funcionalidade de crud de produtos

--- TemplateCleanArch.Application/DTO/ProdutoDTO.cs
+++ TemplateCleanArch.Application/DTO/ProdutoDTO.cs
@@ -12,32 +12,32 @@ namespace TemplateCleanArch.Application.DTO
         [Required(ErrorMessage = "O campo {0} é obrigatório.")]
         [MinLength(3)]
         [MaxLength(200)]
-        [DisplayName("Descricao")]
-        public string Descricao { get; private set; }
+        [DisplayName("Descrição")]
+        public string Descricao { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório.")]
         [MinLength(3)]
         [MaxLength(100)]
         [DisplayName("Nome")]
-        public string Nome { get; private set; }
+        public string Nome { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório.")]
         [Column(TypeName = "decimal(18,2)")]
         [DisplayFormat(DataFormatString = "{0:C2}")]
         [DataType(DataType.Currency)]
         [DisplayName("Preço")]
-        public decimal Preco { get; private set; }
+        public decimal Preco { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório.")]
         [Range(1, 9999)]
         [DisplayName("Estoque")]
-        public int Estoque { get; private set; }
+        public int Estoque { get; set; }
 
         [MaxLength(250)]
         [DisplayName("Imagem do Produto")]
-        public string Imagem { get; private set; }
+        public string Imagem { get; set; }
 
-        public Categoria  Categoria { get; set; }
+        public Categoria Categoria { get; set; }
 
         [DisplayName("Categorias")]
         public int CategoriaId { get; set; }
--- TemplateCleanArch.Web/Controllers/ProdutosController.cs
+++ TemplateCleanArch.Web/Controllers/ProdutosController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.IO;
 using System.Threading.Tasks;
+using TemplateCleanArch.Application.DTO;
 using TemplateCleanArch.Application.Interfaces;
 
 namespace TemplateCleanArch.Web.Controllers
@@ -7,10 +11,14 @@ namespace TemplateCleanArch.Web.Controllers
     public class ProdutosController : Controller
     {
         private readonly IProdutoService _produtoService;
+        private readonly ICategoriaService _categoriaService;
+        private readonly IWebHostEnvironment _environment;
 
-        public ProdutosController(IProdutoService produtoService)
+        public ProdutosController(IProdutoService produtoService, ICategoriaService categoriaService, IWebHostEnvironment environment)
         {
             _produtoService = produtoService;
+            _categoriaService = categoriaService;
+            _environment = environment;
         }   
 
         [HttpGet]
@@ -18,6 +26,84 @@ namespace TemplateCleanArch.Web.Controllers
         {
             var produtos = await _produtoService.BuscarProdutosAsync();
             return View(produtos);
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> Inserir()
+        {
+            ViewBag.CategoriaId = new SelectList(await _categoriaService.BuscarCategoriasAsync(), "Id", "Descricao");
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Inserir(ProdutoDTO produtoDto)
+        {
+            if (ModelState.IsValid)
+            {
+                await _produtoService.CriarAsync(produtoDto);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(produtoDto);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Editar (int? id)
+        {
+            if (id == null) return NotFound();
+            var produtoDto = await _produtoService.BuscarPeloIdAsync(id);
+
+            if (produtoDto == null) return NotFound();
+
+            var categorias = await _categoriaService.BuscarCategoriasAsync();
+
+            ViewBag.CategoriaId = new SelectList(categorias, "Id", "Descricao", produtoDto.CategoriaId);
+
+            return View(produtoDto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Editar(ProdutoDTO produtoDto)
+        {
+            if (ModelState.IsValid)
+            {
+                await _produtoService.AtualizarAsync(produtoDto);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(produtoDto);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Deletar(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var produtoDto = await _produtoService.BuscarPeloIdAsync(id);
+
+            if (produtoDto == null) return NotFound();
+
+            return View(produtoDto);
+        }
+
+        [HttpPost(), ActionName("Deletar")]
+        public async Task<IActionResult> ConfirmarDeletar(int id)
+        {
+            await _produtoService.RemoverAsync(id);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Detalhes(int? id)
+        {
+            if (id == null) return NotFound();
+            var produtoDto = await _produtoService.BuscarPeloIdAsync(id);
+
+            if (produtoDto == null) return NotFound();
+
+            var wwwroot = _environment.WebRootPath;
+            var imagem = Path.Combine(wwwroot, "imagens\\" + produtoDto.Imagem);
+            var exists = System.IO.File.Exists(imagem);
+            ViewBag.ImagemExistente = exists;
+
+            return View(produtoDto);
         }
     }
 }
