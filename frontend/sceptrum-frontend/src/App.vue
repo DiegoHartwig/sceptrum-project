c9649b673b0f6f7778b18efbaf2b38195406ed8a
Diego Hartwig <hartwig.diego@gmail.com>
2023-12-24 18:08:21 -0300

    Adicionando menu

--- frontend/sceptrum-frontend/src/App.vue
+++ frontend/sceptrum-frontend/src/App.vue
@@ -1,19 +1,24 @@
 <template>
-<ul>
-  <li>
-    <router-link :to="{name:'home'}">Home</router-link>
-  </li>  
-  <li>
-    <router-link :to="{name:'produtos'}">Produtos</router-link>
-  </li>  
-  <li v-if="!auth.estaAutenticado()">
-    <router-link :to="{name:'login'}">Login</router-link>
-  </li>
-  <li v-else>
-      <button @click="logout">Logout</button>
-  </li>
-</ul>
+  <div class="q-pa-md">
+    <div class="q-gutter-md">
 
+      <!-- Home -->
+      <q-btn flat :to="{name:'home'}" color="secondary" label="Home"/>
+
+      <!-- Produtos -->
+      <q-btn flat :to="{name:'produtos'}" color="primary" label="Produtos"/> 
+      
+      <template v-if="!auth.estaAutenticado()">
+        <!-- Login -->
+        <q-btn flat :to="{name:'login'}" color="deep-orange" label="Login"/>
+      </template>
+      <template v-else>
+        <!-- Logout -->
+        <q-btn flat @click="logout" color="amber" label="Logout"/>  
+      </template>        
+
+    </div>
+  </div>
 <router-view/>
 
 </template>
